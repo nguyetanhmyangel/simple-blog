e940fad6c64e6cee74771ab49fdc15cf00d6f5d2
unknown <nguyetanhmyangel@gmail.com>
2024-04-06 08:48:59 +0700

    commit -b lession/2.5

---
+++ src/SimpleBlog.Core/Domain/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleBlog.Core.Domain.Entities;
+
+[Table("Tags")]
+public class Tag
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string Name { get; set; }
+}
