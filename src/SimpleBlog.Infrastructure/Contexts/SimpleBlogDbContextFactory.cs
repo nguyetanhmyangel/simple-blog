b2d848b8968557f18196ad4d6e2b965544fddb72
unknown <nguyetanhmyangel@gmail.com>
2024-04-06 10:31:03 +0700

    commit lession/2.7

---
+++ src/SimpleBlog.Infrastructure/Contexts/SimpleBlogDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleBlog.Infrastructure.Contexts;
+
+public class SimpleBlogDbContextFactory : IDesignTimeDbContextFactory<SimpleBlogDbContext>
+{
+    public SimpleBlogDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+        var builder = new DbContextOptionsBuilder<SimpleBlogDbContext>();
+        builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        return new SimpleBlogDbContext(builder.Options);
+    }
+}
